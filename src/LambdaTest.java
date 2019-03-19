9df80fb6e8c7f97ab3acc25a680e7d04e2f79f7e
Husain Chopdawala <h.chopdawala@newcastle.ac.uk>
2019-03-19 20:36:38 +0000

    added a method

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -18,7 +19,22 @@ public class LambdaTest {
         c.add(s);
        var numberOfPlayers= c.stream().filter(p -> p.getTitles()>75).count();
        assertEquals(2,numberOfPlayers);
+    }
+
+    @Test
+    public void methodReferenceTest()
+    {
+        Person a =new Person("Novak","Djokovic");
+        Person b =new Person("Roger","Federer");
+        Person s =new Person("Rafael","Nadal");
+        var c =new ArrayList<Person>();
+        c.add(a);
+        c.add(b);
+        c.add(s);
+        List allAges = Person.listAllAges(c, Person::getTitles);
+        assertEquals(73,allAges.get(0));
 
     }
+
 
 }
--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,9 @@
+import java.time.LocalDate;
+import java.time.Period;
+import  java.util.List;
+import java.util.ArrayList;
+import java.util.function.Function;
+
 /**
  * @author:Husain Chopdawala
  * @desc:represents
@@ -7,38 +13,44 @@ public class Person {
     private String firstName;
     private String lastName;
     private int titles;
-    public Person(String firstName,String lastName)
-    {
-        this.firstName=firstName;
-        this.lastName=lastName;
-        if(this.getLastName().equals("Federer"))
-        {
-            this.titles=100;
-        }
-        else if(this.getLastName().equals("Nadal"))
-        {
-            this.titles=80;
-        }
-        else if(this.getLastName().equals("Djokovic"))
-        {
-            this.titles=73;
-        }
-        else
-        {
-            this.titles=0;
+   
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        if (this.getLastName().equals("Federer")) {
+            this.titles = 100;
+        } else if (this.getLastName().equals("Nadal")) {
+            this.titles = 80;
+        } else if (this.getLastName().equals("Djokovic")) {
+            this.titles = 73;
+        } else {
+            this.titles = 0;
         }
     }
 
-    public String getFirstName()
-    {
+    public String getFirstName() {
         return firstName;
     }
-    public String getLastName()
-    {
+
+    public String getLastName() {
         return lastName;
     }
-    public int getTitles()
-    {
+
+    public int getTitles() {
         return titles;
+    }
+
+    public int getAge(LocalDate birthDate) {
+        if (birthDate != null) {
+            return Period.between(birthDate, LocalDate.now()).getYears();
+        } else {
+            return 0;
+        }
+    }
+
+    public static List listAllAges(List person, Function<Person, Integer> f) {
+        List result = new ArrayList();
+        person.forEach(x -> result.add(f.apply((Person) x)));
+        return result;
     }
 }
